06e37ad4af41989e234f369cf3dc10389dc4f571
Pavel Vesnin <paul.bardack@gmail.com>
2019-04-09 19:28:01 +0300

    fix pre-start release task prefix

--- bin/hooks/pre-start.sh
+++ bin/hooks/pre-start.sh
@@ -4,10 +4,10 @@ APP_NAME="rpc"
 
 if [ "${DB_MIGRATE}" == "true" ]; then
   echo "[WARNING] Migrating database!"
-  ./bin/$APP_NAME command Elixir.Core.ReleaseTasks migrate
+  ./bin/$APP_NAME command Elixir.RPC.ReleaseTasks migrate
 fi;
 
 if [ "${DB_SEED}" == "true" ]; then
   echo "[WARNING] Seeding database!"
-  ./bin/$APP_NAME command Elixir.Core.ReleaseTasks seed
+  ./bin/$APP_NAME command Elixir.RPC.ReleaseTasks seed
 fi;
